9d47c4fdbad82ecfd0a36331c77c00ca25def524
TRTL <4seven@gmail.com>
2023-03-24 11:29:28 +0200

    added image name, author, favorite button

--- js/script.js
+++ js/script.js
@@ -6,10 +6,13 @@ window.onload = function () {
     getFlickrImages(currentPage);
 };
 
-let flickrUrl = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=10a670436576f33e7947d59f5ccf2778&format=json&nojsoncallback=1&`;
+let user = "Karolis";
+
+let flickrUrl = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=d39f3cddeca89fe2c08b0fbd56691c5e&format=json&nojsoncallback=1&`;
 let tags = 'dog';
 let perPage = 24;
 let currentPage = 1;
+
 
 function getFlickrImages(page) {
     fetch(flickrUrl +
@@ -20,11 +23,42 @@ function getFlickrImages(page) {
         .then(data => {
             console.log(data);
             data.photos.photo.forEach(item => {
-                const imgContainer = document.createElement('div');
-                const img = document.createElement('img');
+                // container
+                let container = document.createElement('div');
+                container.classList.add('image-container');
+
+                // img
+                let img = document.createElement('img');
                 img.src = `https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}.jpg`;
-                imgContainer.appendChild(img);
-                document.getElementById('mainContainer').appendChild(imgContainer);
+                container.appendChild(img);
+
+                // info
+                let info = document.createElement('div');
+                info.classList.add('info-container');
+
+                // title
+                let title = document.createElement('span');
+                title.textContent = `${item.title}`;
+                info.appendChild(title);
+
+                // separator
+                let separator = document.createElement('span');
+                separator.classList.add('separator');
+                info.appendChild(separator);
+
+                // author
+                let author = document.createElement('span');
+                author.textContent = `${item.owner}`;
+                info.appendChild(author);
+
+                // button
+                let button = document.createElement('button');
+                button.textContent = 'Favorite';
+                button.classList.add('favorite-button');
+                info.appendChild(button);
+
+                container.appendChild(info);
+                document.getElementById('main-container').appendChild(container);
             });
         })
         .catch(error => {
@@ -33,7 +67,7 @@ function getFlickrImages(page) {
 }
 
 window.addEventListener('scroll', () => {
-    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+    let { scrollTop, scrollHeight, clientHeight } = document.documentElement;
     if (scrollTop + clientHeight >= scrollHeight - 5) {
         currentPage++;
         getFlickrImages(currentPage);
